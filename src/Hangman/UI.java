16442af4d9f584cc634b5665dcc315b6c90e5188
90303054 <90303054@hs-03404-macair.ad.edenpr.org>
2018-05-30 07:52:24 -0500

    a

--- src/Hangman/UI.java
+++ src/Hangman/UI.java
@@ -48,9 +48,11 @@ public class UI extends JFrame {
 	private JButton playagain;
 	private String filename;
 	private JComboBox comboBox;
-	
+
 	private String name;
 	private int numCorrect;
+	private JScrollPane scrollPane;
+	private JTextPane txtpnTheWordTo;
 
 	/**
 	 * Launch the application.
@@ -68,32 +70,39 @@ public class UI extends JFrame {
 			}
 		});
 	}
-	
 
 	/**
 	 * Create the frame.
-	 * @throws IOException 
+	 * 
+	 * @throws IOException
 	 */
 	public UI() throws IOException {
-		this.name = "default";
-		this.numCorrect = 0;
-		leaderboard(name, numCorrect);
+		contentPane = new JPanel();
+		pStart = new JPanel();
+		bStart = new JButton("Start");
+		lTitle = new JLabel("Hangman\n");
+		bQuit = new JButton("Quit");
+		playagain = new JButton("Play again?");
+		btnNewButton = new JButton("Rules");
+		pGame = new JPanel();
+		String[] items = { "words.txt", "countries.txt", "computer.txt", "food.txt", "school.txt" , "out.txt" };
+		comboBox = new JComboBox(items);
+		word = new JLabel("");
+		bpanel = new JPanel();
+		bBack = new JButton("Back");
+		hangingImage = new JLabel();
+		progress = new Label();
+		pRules = new JPanel();
+		btnBack = new JButton("Back");
+		scrollPane = new JScrollPane();
+		txtpnTheWordTo = new JTextPane();
 		initContainers();
 		actions();
 	}
-	
+
 	/**
 	 * Adds to leaderboard the information obtained from the game.
 	 */
-	public void leaderboard(String player, int solved) throws IOException {
-		HangmanLeaderboard hl = new HangmanLeaderboard(player, solved);
-		String fileName = "HangmanLeaderboard.txt";
-		//read existing file
-		ArrayList<String> lead = hl.readFile(fileName);
-		hl.addWordsSolved(lead);
-		hl.writeFile(lead);
-		hl.printArrayList(lead);
-	}
 
 	public void makeButtons() {
 		// TODO Auto-generated method stub
@@ -130,12 +139,6 @@ public class UI extends JFrame {
 						bpanel.setVisible(false);
 						playagain.setVisible(true);
 						try {
-							addLeaderBoard();
-						} catch (IOException e2) {
-							// TODO Auto-generated catch block
-							e2.printStackTrace();
-						}
-						try {
 							h.playSound("img/win.wav");
 						} catch (LineUnavailableException | UnsupportedAudioFileException | IOException e1) {
 							// TODO Auto-generated catch block
@@ -144,7 +147,7 @@ public class UI extends JFrame {
 					}
 				}
 			});
-			
+
 		}
 	}
 
@@ -155,74 +158,64 @@ public class UI extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 900, 600);
 
-		contentPane = new JPanel();
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		/*
 		 * Creates the start JPanel and adds components.
 		 */
 
-		pStart = new JPanel();
 		pStart.setBackground(new Color(25, 25, 112));
 		pStart.setBounds(0, 0, 900, 600);
 		contentPane.add(pStart);
 		pStart.setLayout(null);
-		bStart = new JButton("Start");
+		
 		bStart.setFont(new Font("Copperplate", Font.BOLD, 18));
-		bStart.setBackground(new Color(255, 255, 255));
 		bStart.setBounds(300, 147, 300, 100);
 		pStart.add(bStart);
-		lTitle = new JLabel("Hangman\n");
+
 		lTitle.setForeground(new Color(255, 255, 255));
 		lTitle.setBounds(175, 6, 565, 139);
 		pStart.add(lTitle);
 		lTitle.setHorizontalAlignment(SwingConstants.CENTER);
 		lTitle.setFont(new Font("Wawati SC", Font.BOLD | Font.ITALIC, 99));
-		bQuit = new JButton("Quit");
+
 		bQuit.setFont(new Font("Copperplate", Font.BOLD, 16));
 		bQuit.setBounds(300, 403, 300, 100);
 		pStart.add(bQuit);
-		btnNewButton = new JButton("Rules");
+
 		btnNewButton.setFont(new Font("Copperplate", Font.BOLD, 16));
 		btnNewButton.setBounds(300, 280, 300, 100);
 		pStart.add(btnNewButton);
-		String[] items = { "words.txt", "countries.txt", "computer.txt", "food.txt" , "school.txt" };
-		comboBox = new JComboBox(items);
+
 		comboBox.setBounds(365, 246, 176, 27);
 		pStart.add(comboBox);
-		pGame = new JPanel();
+
 		pGame.setBackground(new Color(0, 128, 128));
 		pGame.setEnabled(false);
 		pGame.setBounds(0, 0, 900, 600);
 		contentPane.add(pGame);
 		pGame.setLayout(null);
 
-		playagain = new JButton("Play again?");
-
 		playagain.setBounds(373, 450, 117, 29);
 		pGame.add(playagain);
 		playagain.setVisible(false);
 
-		word = new JLabel("");
 		word.setFont(new Font("Lucida Grande", Font.PLAIN, 25));
 		word.setBounds(373, 400, 414, 61);
 		pGame.add(word);
 
-		bpanel = new JPanel();
 		bpanel.setBackground(new Color(0, 128, 128));
 		bpanel.setBounds(100, 416, 703, 270);
 		pGame.add(bpanel);
-		bBack = new JButton("Back");
+
 		bBack.setFont(new Font("Copperplate", Font.BOLD, 16));
 		bBack.setBounds(710, 37, 173, 61);
 		pGame.add(bBack);
 		pGame.setVisible(false);
 
-		hangingImage = new JLabel();
 		hangingImage.setBounds(150, 37, 687, 315);
 		pGame.add(hangingImage);
 
-		progress = new Label();
 		progress.setAlignment(Label.CENTER);
 		progress.setFont(new Font("Comic Sans MS", Font.PLAIN, 40));
 		progress.setBounds(10, 332, 873, 76);
@@ -230,31 +223,25 @@ public class UI extends JFrame {
 		/*
 		 * Creates JPanel rules and creates buttons and text box.
 		 */
-		pRules = new JPanel();
+
 		pRules.setBackground(new Color(123, 104, 238));
 		pRules.setBounds(0, 0, 900, 600);
 		contentPane.add(pRules);
 		pRules.setLayout(null);
-		btnBack = new JButton("Back");
+
 		btnBack.setFont(new Font("Copperplate", Font.BOLD, 14));
 		btnBack.setBounds(349, 420, 190, 91);
 		pRules.add(btnBack);
-		JScrollPane scrollPane = new JScrollPane();
+
 		scrollPane.setBounds(6, 6, 888, 370);
 		pRules.add(scrollPane);
-		JTextPane txtpnTheWordTo = new JTextPane();
+
 		txtpnTheWordTo.setFont(new Font("Comic Sans MS", Font.BOLD, 16));
 		txtpnTheWordTo.setEditable(false);
 		txtpnTheWordTo.setText(
 				"The word to guess is represented by a row of dashes, representing each letter of the word. If the guessing player suggests a letter which occurs in the word, the other player writes it in all its correct positions. If the suggested letter or number does not occur in the word, the other player draws one element of a hanged man stick figure as a tally mark.\nThe player guessing the word may, at any time, attempt to guess the whole word. If the word is correct, the game is over and the guesser wins. Otherwise, the other player may choose to penalize the guesser by adding an element to the diagram. On the other hand, if the other player makes enough incorrect guesses to allow his opponent to complete the diagram, the game is also over, this time with the guesser losing. However, the guesser can also win by guessing all the letters or numbers that appears in the word, thereby completing the word, before the diagram is completed.\nIn our version of Hangman, you want to accumulate the most points as possible. Make sure to watch the Eclipse Counsole for in-game messages!\n\nGood Luck!");
 		scrollPane.setViewportView(txtpnTheWordTo);
 		pRules.setVisible(false);
-		/*
-		 * Creates the JPanel pGame and adds components.
-		 */
-		/*
-		 * Creates JPanel bpanel and calls makeButtons for the button array.
-		 */
 		makeButtons();
 
 	}
@@ -325,8 +312,6 @@ public class UI extends JFrame {
 		}
 		playagain.setVisible(false);
 	}
-	public void addLeaderBoard() throws IOException{
-		numCorrect++;
-		leaderboard(name, numCorrect);
-	}
+
+	
 }
